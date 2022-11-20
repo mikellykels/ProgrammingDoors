57c769850733b3854e6c6d45efcd8a3d062ddc37
Kelly Carino <mikaela.carino@gmail.com>
2022-11-20 02:49:11 -0800

    add base files

---
+++ Source/ProgrammingDoors/ProgrammingDoors.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProgrammingDoors.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProgrammingDoors, "ProgrammingDoors" );
